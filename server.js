9814ec274dedde95b38195d7df501c583ed0225f
Andrew Hartzell <hartwrm@Macs-MacBook-Pro.local>
2019-06-29 12:02:10 -0400

    connected to server

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+const PORT = 3003
+const mongoose = require('mongoose')
+const cors = require('cors')
+
+
+
+
+// Error / Disconnection
+mongoose.connection.on('error', err => console.log(err.message + ' is Mongod not running?'))
+mongoose.connection.on('disconnected', () => console.log('mongo disconnected'))
+
+mongoose.connect('mongodb://localhost:27017/kickstART', { useNewUrlParser: true })
+mongoose.connection.once('open', ()=>{
+    console.log('connected to mongoose...')
+})
+
+
+
+//listening
+app.listen(PORT, () => {
+  console.log("Ground  Control to Major Tom on port:", PORT);
+})
