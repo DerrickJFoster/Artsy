2e371d17f4dfc22f9fdb4390ac913382531d93f2
Andrew Hartzell <hartwrm@Macs-MacBook-Pro.local>
2019-07-02 19:45:56 -0400

    return userid and username

--- controllers/users.js
+++ controllers/users.js
@@ -5,12 +5,33 @@ const bcrypt = require('bcrypt')
 
 //create user
 router.post('/', (req, res) => {
+  req.body.password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10))
   User.create(req.body, (error, newUser) => {
     if (error) {
       res.status(400).json({error: error.message})
     }
-    res.status(200).json(newUser._id)
+    res.status(200).json(
+      {
+        id:newUser._id,
+        username: newUser.username
+    }
+  )
   })
 })
+
+//log in
+// router.post('/login', (req, res) => {
+//   User.findOne({username: request.body.username}, (err, foundUser) => {
+//     if (err) {
+//       console.log(err)
+//       res.status(400).json({error: err.message})
+//     } else if (!foundUser) {
+//       res.status(400).json({error: err.message})
+//     } else if (bcrypt.compareSync(req.body.password, foundUser.password)) {
+//       req.body.username: foundUser.username
+//     }
+//   })
+// })
+
 
 module.exports = router;
--- models/users.js
+++ models/users.js
@@ -8,29 +8,6 @@ const userSchema = new Schema({
   password:{type: String, required: true}
 })
 
-//salting
-const saltRounds = 10
-
-//has pw hased and salted before added to DB
-userSchema.pre('save', function(next){
-  //check if new doc or pw has been set
-  if (this.isNew || this.isModified('password')) {
-    //save reference because of scope changes
-    const document = this;
-    bcrypt.hash(document.password, saltRounds,
-    function(err, hashedPassword) {
-      if (err) {
-        next(err)
-      } else {
-        document.password = hashedPassword;
-        next()
-      }
-    });
-  } else {
-    next();
-  }
-});
-
 const User = mongoose.model('User', userSchema)
 
 module.exports = User
