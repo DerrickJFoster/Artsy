4ae051c3a534f9f51852d2b58c88ed30485e4e6f
Andrew Hartzell <hartwrm@Macs-MacBook-Pro.local>
2019-07-02 19:47:45 -0400

    cors comments

--- server.js
+++ server.js
@@ -28,19 +28,19 @@ mongoose.connection.once('open', ()=>{
 
 
 //CORS
-// const whitelist = ['http://localhost:3000', 'https://derrickjfoster.github.io', 'https://kickstart-me.herokuapp.com/art', 'https://kickstart-me.herokuapp.com'] //heroku link
-// const corsOptions = {
-//   origin: function (origin, callback) {
-//     if (whitelist.indexOf(origin) !== -1) {
-//       callback(null, true)
-//     } else {
-//       callback(new Error('Not allowed by CORS'))
-//     }
-//   }
-// }
+const whitelist = ['http://localhost:3000', 'https://derrickjfoster.github.io', 'https://kickstart-me.herokuapp.com/art', 'https://kickstart-me.herokuapp.com'] //heroku link
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
 
 //Middleware
-// app.use(cors(corsOptions))
+app.use(cors(corsOptions))
 app.use(express.json())
 app.use('/art', artController)
 app.use('/users', usersController)
